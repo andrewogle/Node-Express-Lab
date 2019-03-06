af1213f2814262a5e0023af3194d36c0aca14282
Andrew <andrewryanogle@gmail.com>
2019-03-05 18:36:00 -0800

    finished post in routerJS

--- data/router.js
+++ data/router.js
@@ -2,6 +2,19 @@ const express = require('express');
 const db = require('./db.js');
 const router = express.Router();
 
-
+router.post('/', async (req, res)=>{
+    try{
+        const post = await db.insert(req.body);
+        if(post){
+            res.status(201).json(post)
+        }
+        else{
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        }
+    }
+    catch(error){
+        res.
+    }
+})
 
 module.exports = router;
